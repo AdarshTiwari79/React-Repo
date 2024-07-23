fb12e949bd4110c6089d68d18fd35225cd8909f7
AdarshTiwari79 <jaunpuradarsh62@gmail.com>
2024-07-23 09:49:34 +0530

    button to generate new ticket and condition of winning

--- Task todo list/src/lottery.jsx
+++ Task todo list/src/lottery.jsx
@@ -1,18 +1,27 @@
 import { useState } from "react";
-import { genTicket } from "./assets/helper";
+import { genTicket, sum } from "./assets/helper";
 import "./lottery.css";
 
 function Lottery() {
   let [ticket, setTicket] = useState(genTicket(3));
 
+  let isWinning = sum(ticket) === 15;
+
+  let genNewTicket = () => {
+    setTicket(genTicket(3));
+  };
+
   return (
     <div>
       <h2>Lotter Game</h2>
       <div className="ticket">
-        {ticket[0]}
-        {ticket[1]}
-        {ticket[2]}
+        {ticket.map((el, index) => {
+          return <span key={index}>{el}</span>;
+        })}
       </div>
+      <br></br>
+      <button onClick={genNewTicket}>Buy new ticket</button>
+      {isWinning ? <h3>congratulations you won lottery</h3> : ""}
     </div>
   );
 }
