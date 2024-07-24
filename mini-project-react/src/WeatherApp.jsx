d512a59a00f1773793b1425e495d4091a6c2bb2a
AdarshTiwari79 <jaunpuradarsh62@gmail.com>
2024-07-24 14:01:25 +0530

    deleted default value

--- mini-project-react/src/WeatherApp.jsx
+++ mini-project-react/src/WeatherApp.jsx
@@ -3,15 +3,7 @@ import InfoBox from "./infoBox";
 import SearchBox from "./searchBox";
 
 export default function WeatherApp() {
-  let [weatherInfo, setWetherInfo] = useState({
-    city: "delhi",
-    temp: 25.05,
-    tempMin: 25,
-    tempMax: 26,
-    humidity: 68,
-    feelsLike: 31,
-    weather: "cloudy",
-  });
+  let [weatherInfo, setWetherInfo] = useState({});
 
   let updateInfo = (result) => {
     setWetherInfo(result);
